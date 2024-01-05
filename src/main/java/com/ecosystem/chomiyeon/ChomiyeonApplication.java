c4f7e6795949e8c9d8651d2e365906af2b83c387
bang-dev <ecosystemprogramminglanguage@gmail.com>
2024-01-05 23:36:40 +0700

    Setup Basic Project

---
+++ src/main/java/com/ecosystem/chomiyeon/ChomiyeonApplication.java
@@ -0,0 +1,13 @@
+package com.ecosystem.chomiyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChomiyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChomiyeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecosystem/chomiyeon/ChomiyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecosystem.chomiyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChomiyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
