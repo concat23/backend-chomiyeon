7acda1c7585b84e7deced72c27a7de083ebfd297
bang-dev <ecosystemprogramminglanguage@gmail.com>
2024-01-07 19:03:49 +0700

    Update handle

--- src/main/java/com/ecosystem/chomiyeon/service/ICmyEmployeeService.java
+++ src/main/java/com/ecosystem/chomiyeon/service/ICmyEmployeeService.java
@@ -3,9 +3,13 @@ package com.ecosystem.chomiyeon.service;
 import com.ecosystem.chomiyeon.dto.CmyEmployeeDTO;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ICmyEmployeeService {
     CmyEmployeeDTO createCmyEmployee(CmyEmployeeDTO cmyEmployeeDTO);
     List<CmyEmployeeDTO> listCmyEmployees();
     CmyEmployeeDTO getCmyEmployeeById(Long cmyEmployeeId);
+    CmyEmployeeDTO updateCmyEmployee(Long cmyEmployeeId, Map<String, Object> updateFields);
+
+    void deleteCmyEmployee(Long cmyEmployeeId);
 }
